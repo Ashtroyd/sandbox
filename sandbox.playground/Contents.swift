f3c355ba1d173cc6f91fa63d00073866a9d3a5e4
John-Luke Brooks <johnlukebrooks80@gmail.com>
2026-02-13 09:15:28 +0000

    d1

---
+++ sandbox.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Cocoa
+
+var theme = "knowledge"
+
+let topic = "knowledge"
+
+let quote = """
+    A long time ago in
+    a galaxy far
+    far away...
+    """
+print(quote.count)
+
+print(quote.hasPrefix("A long"))
+print(quote.hasSuffix("away..."))
+let meaningOfLife = topic.count
+print(meaningOfLife)
+
+
